41ca0e8ce8ffea8098973c015f97e1cd9931eee8
MitchelMA <32548@ma-web.nl>
2023-02-18 20:42:32 +0100

    veranderen van waardes in de layout middels AppState.cs

---
+++ Portfolio/Services/AppState.cs
@@ -0,0 +1,36 @@
+﻿namespace Portfolio.Services;
+
+public class AppState
+{
+    private bool _showFooter = true;
+    private string _headerImgPath = "icon-192.png";
+
+    public event Action StateChanged;
+    private void NotifyStateChanged() => StateChanged?.Invoke();
+
+    public bool ShowFooter
+    {
+        get => _showFooter;
+        set
+        {
+            if (value == _showFooter)
+                return;
+
+            _showFooter = value;
+            NotifyStateChanged();
+        }
+    }
+    
+    public string HeaderImgPath
+    {
+        get => _headerImgPath;
+        set
+        {
+            if (value == _headerImgPath)
+                return;
+
+            _headerImgPath = value;
+            NotifyStateChanged();
+        }
+    }
+}
