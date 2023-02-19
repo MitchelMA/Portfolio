26987423346f2923a76beb2349d4400841347b6f
MitchelMA <32548@ma-web.nl>
2023-02-19 20:57:04 +0100

    kleine aanpassingen en AnchorLink.razor om naar een locale id-href te scrollen

---
+++ Portfolio/Services/LinkHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Portfolio.Services;
+
+public static class LinkHelper
+{
+    public static string AsLocalHref(string href, NavigationManager manager)
+    {
+        string ret = href;
+        if (href.StartsWith("#"))
+            ret = manager.ToAbsoluteUri(manager.Uri).GetLeftPart(UriPartial.Path) + ret;
+
+        return ret;
+    }
+}
--- Portfolio/wwwroot/js/scroll.js
+++ Portfolio/wwwroot/js/scroll.js
@@ -7,3 +7,24 @@ function scrollToTop()
 {
     document.documentElement.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"})
 }
+
+function scrollToId(id, topOffset = 50)
+{
+    let elem = document.getElementById(id);
+    if(!elem)
+        return;
+    
+    // elem.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+    scrollToElem(elem, topOffset);
+}
+
+function scrollToElem(element, topOffset)
+{
+    let elementPosition = element.getBoundingClientRect().top;
+    let offsetPosition = elementPosition + window.scrollY - topOffset;
+    
+    document.documentElement.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth"
+    });
+}
