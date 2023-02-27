28bc6ad412f72ba680eb54d7757dcb81b2aa2186
MitchelMA <vonkm124@gmail.com>
2023-02-27 14:20:58 +0100

    page-title extensions

--- Portfolio/Model/PageDetails.cs
+++ Portfolio/Model/PageDetails.cs
@@ -5,6 +5,7 @@ namespace Portfolio.Model;
 
 public class PageDetails
 {
+    public string TitleExtension = string.Empty;
     public LinkTag Icon = StaticData.DefaultPageIcon;
     public bool ShowFooter = true;
     public (string, string)[]? Links;
