bfe358a5597181979cfe281a7edb8af2a5056f74
MitchelMA <vonkm124@gmail.com>
2023-03-13 17:58:23 +0100

    speciaal page-end voor project pages

---
+++ Portfolio/Model/ProjectPageEnd.cs
@@ -0,0 +1,6 @@
+namespace Portfolio.Model;
+
+public class ProjectPageEnd
+{
+    public string? GitHubLink { get; set; }
+}
--- Portfolio/Services/ProjectState.cs
+++ Portfolio/Services/ProjectState.cs
@@ -1,10 +1,12 @@
 using Portfolio.Enums;
+using Portfolio.Model;
 
 namespace Portfolio.Services;
 
 public class ProjectState
 {
     private ProjectStatus _projectStatus;
+    private ProjectPageEnd _projectPageEnd = new();
 
     public event Action? StateChanged;
     public event Func<Task>? StateChangedAsync;
@@ -24,6 +26,32 @@ public class ProjectState
                 return;
 
             _projectStatus = value;
+            NotifyStateChanged();
+        }
+    }
+
+    public ProjectPageEnd ProjectPageEnd
+    {
+        get => _projectPageEnd;
+        set
+        {
+            if (value == _projectPageEnd)
+                return;
+
+            _projectPageEnd = value;
+            NotifyStateChanged();
+        }
+    }
+
+    public string? PageEndGithub
+    {
+        get => ProjectPageEnd.GitHubLink;
+        set
+        {
+            if (value == ProjectPageEnd.GitHubLink)
+                return;
+
+            ProjectPageEnd.GitHubLink = value;
             NotifyStateChanged();
         }
     }
