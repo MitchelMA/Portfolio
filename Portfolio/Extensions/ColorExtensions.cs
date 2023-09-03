1a26eeea6c51c41f84f60182ea1dcd886f23cac6
MitchelMA <vonkm124@gmail.com>
2023-09-03 16:17:58 +0200

    Rekening houdend met invariant-culture omdat decimale getallen een comma kunnen hebben en CSS accepteerd deze niet

--- Portfolio/Extensions/ColorExtensions.cs
+++ Portfolio/Extensions/ColorExtensions.cs
@@ -1,9 +1,14 @@
+using System.Diagnostics;
 using System.Drawing;
+using System.Globalization;
 
 namespace Portfolio.Extensions;
 
 public static class ColorExtensions
 {
+    private static string GetPercentageString(byte alpha) =>
+        (alpha / 255f * 100).ToString(CultureInfo.InvariantCulture);
+
     public static string ToCssString(this Color color) =>
-        $"rgb({color.R} {color.G} {color.B} / {color.A / 255f * 100}%)";
+        $"rgb({color.R} {color.G} {color.B} / {GetPercentageString(color.A)}%)";
 }
