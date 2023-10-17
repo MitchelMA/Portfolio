5d763e7f4a7bee15f8e09589166326f2d1f6b2a1
MitchelMA <32548@ma-web.nl>
2023-10-17 11:26:29 +0200

    toevoeging van begin van c-vector pagina plus omgooi van mappers

--- Portfolio/Mappers/IMapper.cs
+++ Portfolio/Mappers/IMapper.cs
@@ -1,9 +1,6 @@
 ﻿namespace Portfolio.Mappers;
 
-public interface IMapper<in T, out T2>
+public interface IMapper
 {
-    public static T2 MapFrom(T obj)
-    {
-        return default!;
-    }
+     abstract object? MapFrom(object? obj);
 }
---
+++ Portfolio/Mappers/ToNavLinkDataMapper.cs
@@ -0,0 +1,20 @@
+﻿using Portfolio.Model;
+using Portfolio.Model.Text;
+
+namespace Portfolio.Mappers;
+
+public class ToNavLinkDataMapper : IMapper<LangLinkModel, NavLinkData>, IMapper
+{
+    public NavLinkData MapFrom(LangLinkModel obj)
+    {
+        return new NavLinkData(obj.Href, obj.DisplayText, obj.OpensNew);
+    }
+
+    public object? MapFrom(object? obj)
+    {
+        if (obj is not LangLinkModel typedObj)
+            return default;
+
+        return MapFrom(typedObj);
+    }
+}
