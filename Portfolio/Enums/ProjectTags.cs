b4ea521189ba3a0506d912ff0074c5ae4a46de6a
MitchelMA <vonkm124@gmail.com>
2024-11-16 16:52:09 +0100

    betere display tekst voor de gespecificeerde waardes

--- Portfolio/Enums/ProjectTags.cs
+++ Portfolio/Enums/ProjectTags.cs
@@ -36,23 +36,39 @@ public enum ProjectTags
     [EnumMember(Value = "Library")]
     Lib = 1 << 11,
     
+    [EnumMember(Value = "Unity Project")]
     UnityProject = Unity | CSharp,
+    [EnumMember(Value = "Finished Unity Project")]
     UnityFinished = Finished | UnityProject,
+    [EnumMember(Value = "Develop Unity Project")]
     UnityInDev = InDevelopment | UnityProject,
+    [EnumMember(Value = "Prototype Unity Project")]
     UnityPrototype = Prototype | UnityProject,
     
+    [EnumMember(Value = "C Project")]
     CProject = CMake | C,
+    [EnumMember(Value = "Finished C Project")]
     CFinished = Finished | CProject,
+    [EnumMember(Value = "Develop C Project")]
     CInDev = InDevelopment | CProject,
+    [EnumMember(Value = "Prototype C Project")]
     CPrototype = Prototype | CProject,
     
+    [EnumMember(Value = "C++ Project")]
     CxxProject = CMake | Cxx,
+    [EnumMember(Value = "Finished C++ Project")]
     CxxFinished = Finished | CxxProject,
+    [EnumMember(Value = "Develop C++ Project")]
     CxxInDev = InDevelopment | CxxProject,
+    [EnumMember(Value = "Prototype C++ Project")]
     CxxPrototype = Prototype | CxxProject,
     
+    [EnumMember(Value = "C# Project")]
     CSharpProject = DotNet | CSharp,
+    [EnumMember(Value = "Finished C# Project")]
     CSharpFinished = Finished | CSharpProject,
+    [EnumMember(Value = "Develop C# Project")]
     CSharpInDev = InDevelopment | CSharpProject,
+    [EnumMember(Value = "Prototype C# Project")]
     CSharpPrototype = Prototype | CSharpProject,
 }
