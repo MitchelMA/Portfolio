b496094686522b72f8ffe7db273f91458a5a8352
MitchelMA <vonkm124@gmail.com>
2024-11-23 14:24:04 +0100

    Project layout verbeterd via het gebruik van cascading values

---
+++ Portfolio/Shared/Layouts/ProjectLayout.razor.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using Portfolio.Client;
+using Portfolio.Model.Project;
+using Portfolio.Services;
+
+namespace Portfolio.Shared.Layouts;
+
+public partial class ProjectLayout : LayoutComponentBase, IDisposable
+{
+    [Inject]
+    private AppState? AppState { get; init; }
+    [Inject]
+    private IJSRuntime? JsRuntime { get; init; }
+    
+    private ProjectDataModel? _model;
+    public ProjectDataModel? Model
+    {
+        get => _model;
+        set
+        {
+            _model = value;   
+            StateHasChanged();
+        }
+    }
+
+    protected override void OnInitialized()
+    {
+        AppState!.StateChangedAsync += AppStateChanged;
+    }
+
+    private async Task AppStateChanged()
+    {
+        await CheckScroll();
+        StateHasChanged();
+    }
+
+    private ValueTask CheckScroll()
+    {
+        return JsRuntime!.InvokeVoidAsync(AppState!.ScrollLocked ? "lockScroll" : "unlockScroll", StaticData.LockedClassName);
+    }
+    
+    public void Dispose()
+    {
+        AppState!.StateChangedAsync -= AppStateChanged;
+        GC.SuppressFinalize(this);
+    }
+}
